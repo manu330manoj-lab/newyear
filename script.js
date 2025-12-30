23f399819db989a4aa64c4f18d262ea4ba6bfde6
manu330manoj-lab <manu330.manoj@gmail.com>
2025-12-30 12:16:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,110 @@ btn.addEventListener("click", (e) => {
 
   isPlaying = !isPlaying;
 });
+
+const canvas = document.getElementById("fireworks");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+function random(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+class Firework {
+  constructor() {
+    this.x = random(0, canvas.width);
+    this.y = canvas.height;
+    this.targetY = random(canvas.height * 0.2, canvas.height * 0.5);
+    this.radius = 2;
+    this.color = `hsl(${random(0,360)},70%,70%)`;
+    this.speed = random(3, 5);
+    this.exploded = false;
+    this.particles = [];
+  }
+
+  update() {
+    if (!this.exploded) {
+      this.y -= this.speed;
+      if (this.y <= this.targetY) {
+        this.explode();
+      }
+    } else {
+      this.particles.forEach(p => p.update());
+    }
+  }
+
+  explode() {
+    this.exploded = true;
+    for (let i = 0; i < 30; i++) {
+      this.particles.push(new Particle(this.x, this.y, this.color));
+    }
+  }
+
+  draw() {
+    if (!this.exploded) {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+      ctx.fillStyle = this.color;
+      ctx.fill();
+    } else {
+      this.particles.forEach(p => p.draw());
+    }
+  }
+}
+
+class Particle {
+  constructor(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.radius = 2;
+    this.speedX = random(-3, 3);
+    this.speedY = random(-3, 3);
+    this.life = 60;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    this.life--;
+  }
+
+  draw() {
+    if (this.life > 0) {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+      ctx.fillStyle = this.color;
+      ctx.fill();
+    }
+  }
+}
+
+let fireworks = [];
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  if (Math.random() < 0.04) {
+    fireworks.push(new Firework());
+  }
+
+  fireworks.forEach((fw, index) => {
+    fw.update();
+    fw.draw();
+    if (fw.exploded && fw.particles.every(p => p.life <= 0)) {
+      fireworks.splice(index, 1);
+    }
+  });
+
+  requestAnimationFrame(animate);
+}
+
+animate();
+
